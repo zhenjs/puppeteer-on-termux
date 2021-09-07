8c5ce0bc237f7d13d9c7ab297c52c2c779556aec
anzhen <anzhen@kuaishou.com>
2021-09-07 18:27:48 +0800

    asdf

--- index.js
+++ index.js
@@ -13,14 +13,21 @@ async function screenshot(url) {
   });
 
   const page = await browser.newPage();
-  await page.setViewport({width: 1920, height: 1080});
+  await page.setViewport({
+    width: 1920,
+    height: 1080
+  });
+  await page.tracing.start({
+    path: 'trace.json'
+  });
   await page.goto(url, {
     timeout: 0,
     waitUntil: 'networkidle0',
   });
-  const screenData = await page.screenshot({encoding: 'binary', type: 'jpeg', quality: 100});
-  if (!!screenData) {
-    fs.writeFileSync('screenshots/screenshot.jpg', screenData);
+  const tracing = await page.tracing.stop();
+  // const screenData = await page.screenshot({encoding: 'binary', type: 'jpeg', quality: 100});
+  if (!!tracing) {
+    fs.writeFileSync('screenshots/tracing.json', tracing);
   } else {
     throw Error('Unable to take screenshot');
   }
